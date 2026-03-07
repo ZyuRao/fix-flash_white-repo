b46b98129b13f1d843edc993c44a06d83bae0073
Ethan <2300011177@stu.pku.edu.cn>
2026-03-07 17:21:29 +0800

    feat: add FriendLinkCard component and FriendLinkItem interface for dynamic friend links

---
+++ src/config/links.ts
@@ -0,0 +1,9 @@
+export interface FriendLinkItem {
+  name: string;
+  description: string;
+  href: string;
+  avatarSrc: string;
+}
+
+export const friendLinks = [] satisfies readonly FriendLinkItem[];
+
